bf185dea3315fd57a6819bec11790d689f74cef1
James Phillip Mayor <117265110+JpMayor1@users.noreply.github.com>
2024-02-02 00:29:05 +0800

    indigency UI

--- src/app/[barangay]/(admin)/certificates/indigency/page.tsx
+++ src/app/[barangay]/(admin)/certificates/indigency/page.tsx
@@ -1,136 +1,156 @@
-'use client'
+"use client";
 
-import { Button, Input } from "@material-tailwind/react"
-import dayjs from "dayjs"
-import { renderAsync } from "docx-preview"
-import { useEffect, useRef, useState } from "react"
-
-const nthNumber = (number: number) => {
-  if (number > 3 && number < 21) return "th";
-  switch (number % 10) {
-    case 1:
-      return "st";
-    case 2:
-      return "nd";
-    case 3:
-      return "rd";
-    default:
-      return "th";
-  }
-};
+import { Button, Input } from "@material-tailwind/react";
+import dayjs from "dayjs";
+import { renderAsync } from "docx-preview";
+import Image from "next/image";
+import { useEffect, useRef, useState } from "react";
+import logo from "../../../../../../public/logo.png";
+import daetLogo from "../../../../../../public/daet-logo.png";
+import footer from "../../../../../../public/footer.png";
+import dahon from "../../../../../../public/dahon.jpg";
 
 function Indigency() {
-  const [form, setForm] = useState<{}>({ year: 2024 })
-  const rendererRef = useRef(null)
-
-  const [docx, setDocx] = useState<any | null>(null)
-
-  useEffect(() => {
-    if (rendererRef.current) {
-      renderAsync(Buffer.from(docx), rendererRef.current).then((res) => {
-        console.log('done');
-      })
-    }
-
-  }, [docx])
-
-  const Generate = async () => {
-    const res = await fetch('/api/documents', {
-      method: 'POST',
-      body: JSON.stringify({
-        type: 'indigency',
-        data: form
-      })
-    })
-
-    if (res.status === 200) {
-      const { buffer }: { buffer: Buffer } = await res.json();
-      console.log('buffered');
-      setDocx(buffer);
-    }
-
-
-  }
-
-  if (docx !== null) {
     return (
-      <div ref={rendererRef} className="w-full h-full rounded-xl shadow-md bg-white">
+        <div className="w-full h-fit flex justify-center items-center">
+            <div className="printing-view w-[816px] h-[1056px] bg-white p-10">
+                <div className="w-full h-full border-4 border-green-500 p-[1px]">
+                    <div className="w-full h-full border border-green-500 relative">
+                        <div className="w-full flex items-center justify-center gap-6 mt-12">
+                            <div className="w-[100px] h-[100px]">
+                                <Image
+                                    src={logo}
+                                    alt="Logo"
+                                    className="h-full w-full"
+                                />
+                            </div>
 
-      </div>
-    )
-  }
+                            <div className="text-center text-[15px] font-tahoma ">
+                                <p>Republic of the Philippines</p>
+                                <p>Province of Camarines Norte</p>
+                                <p>MUNICIPALITY OF DAET</p>
+                                <p>
+                                    <b>BARANGAY BORABOD</b>
+                                </p>
+                            </div>
 
-  return (
-    <div className="w-full h-full p-10 flex flex-col justify-center items-center rounded-xl shadow-lg bg-white">
-      <div className="w-1/2 text-center text-3xl my-5 font-bold">
-        BARANGAY INDIGENCY
-      </div>
-      <div className="w-1/2">
-        <Input
-          label="Name"
-          onChange={(ev) => setForm((prev) => ({ ...prev, name: ev.target.value }))}
-        />
-      </div>
-      <div className="w-1/2 flex flex-row my-3 gap-x-5">
-        <div className="flex-auto">
-          <Input
-            label="Age"
-            onChange={(ev) => setForm((prev) => ({ ...prev, age: ev.target.value }))}
-          />
+                            <div className="w-[100px] h-[100px]">
+                                <Image
+                                    src={daetLogo}
+                                    alt="Daet Logo"
+                                    className="h-full w-full"
+                                />
+                            </div>
+                        </div>
+
+                        <div className="mt-12">
+                            <h1 className="text-center text-xl font-bold">
+                                OFFICE OF THE SANGGUNIANG BARANGAY
+                            </h1>
+                        </div>
+
+                        <div className="mt-5">
+                            <h1 className="text-center text-3xl font-old-english underline">
+                                Certificate of Indigency
+                            </h1>
+                        </div>
+
+                        <div className="mt-5 px-14 font-cambria text-[16px]">
+                            <p>TO WHOM IT MAY CONCERN:</p>
+                            <p className="mt-5">
+                                This is to certify that{" "}
+                                <input
+                                    type="text"
+                                    name="name"
+                                    className="text-center border-b-2 border-black w-[150px] outline-none bg-transparent"
+                                />
+                                ,{" "}
+                                <input
+                                    type="text"
+                                    name="age"
+                                    className="text-center border-b-2 border-black w-7 outline-none"
+                                />{" "}
+                                years of age,{" "}
+                                <input
+                                    type="text"
+                                    name="status"
+                                    className="text-center border-b-2 border-black w-16 outline-none"
+                                />
+                                , a bonafide resident of Purok{" "}
+                                <input
+                                    type="text"
+                                    name="purok"
+                                    className="text-center border-b-2 border-black w-5 outline-none"
+                                />
+                                , Barangay Borabod Daet, Camarines Norte.
+                            </p>
+                            <p className="mt-5">
+                                Furthermost, it is here then to certify as per
+                                record of the Barangay thru Census that{" "}
+                                <input
+                                    type="text"
+                                    name="name"
+                                    className="text-center border-b-2 border-black w-[150px] outline-none bg-transparent"
+                                />{" "}
+                                belongs to the indigent family of this locality.
+                            </p>
+                            <p className="mt-5">
+                                Issued this{" "}
+                                <input
+                                    type="text"
+                                    name="day"
+                                    className="text-center border-b-2 border-black w-5 outline-none"
+                                />{" "}
+                                day of{" "}
+                                <input
+                                    type="text"
+                                    name="month"
+                                    className="text-center border-b-2 border-black w-20 outline-none"
+                                />
+                                ,{" "}
+                                <input
+                                    type="text"
+                                    name="year"
+                                    className="text-center border-b-2 border-black w-20 outline-none"
+                                />{" "}
+                                at Barangay Hall Borabod Daet, Camarines Norte
+                                in connection with his/her application
+                                requirement for{" "}
+                                <input
+                                    type="text"
+                                    name="purpose"
+                                    className="text-center border-b-2 border-black w-[150px] outline-none bg-transparent"
+                                />{" "}
+                                of his/her common in law{" "}
+                                <input
+                                    type="text"
+                                    name="relative"
+                                    className="text-center border-b-2 border-black w-[150px] outline-none bg-transparent"
+                                />
+                                .
+                            </p>
+                            <p className="mt-5">Certified by:</p>
+                        </div>
+                        {/* footer */}
+                        <div className="w-full absolute left-0 bottom-8 z-10">
+                            <Image
+                                src={footer}
+                                alt="Footer"
+                                className="h-full w-full"
+                            />
+                        </div>
+                        <div className="w-[121px] h-[554px] absolute right-1 bottom-8">
+                            <Image
+                                src={dahon}
+                                alt="dahon"
+                                className="h-full w-full"
+                            />
+                        </div>
+                    </div>
+                </div>
+            </div>
         </div>
-        <div className="flex-auto">
-          <Input
-            label="Marital Status"
-            onChange={(ev) => setForm((prev) => ({ ...prev, status: ev.target.value }))}
-          />
-        </div>
-        <div className="flex-auto">
-          <Input
-            label="Purok"
-            onChange={(ev) => setForm((prev) => ({ ...prev, purok: ev.target.value }))}
-          />
-        </div>
-      </div>
-      <div className="w-1/2 flex flex-row my-3 gap-x-5">
-        <div className="flex-auto">
-          <Input
-            type="number"
-            min={1}
-            max={31}
-            label="Day Issued"
-            onChange={(ev) => setForm((prev) => ({ ...prev, day: `${ev.target.value}${nthNumber(Number(ev.target.value))}` }))}
-          />
-        </div>
-        <div className="flex-auto">
-          <Input
-            type="text"
-            label="Month Issued"
-            onChange={(ev) => setForm((prev) => ({ ...prev, month: ev.target.value }))}
-          />
-        </div>
-        <div className="flex-auto">
-          <Input
-            type="number"
-            label="Year Issued"
-            defaultValue={new Date().getFullYear()}
-            min={new Date().getFullYear()}
-            onChange={(ev) => setForm((prev) => ({ ...prev, year: ev.target.value }))}
-          />
-        </div>
-      </div>
-      <div className="w-1/2">
-        <Input
-          label="Purpose"
-          onChange={(ev) => setForm((prev) => ({ ...prev, purpose: ev.target.value }))}
-        />
-      </div>
-      <div className="w-1/2 my-3 text-end">
-        <Button onClick={() => Generate()}>
-          <span>Generate Result</span>
-        </Button>
-      </div>
-    </div>
-  )
+    );
 }
 
-export default Indigency
+export default Indigency;
